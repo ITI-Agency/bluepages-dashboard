daac1c1bdefa84b82e5e41c6d6f352681dd92c74
Selim-Dev <alisleem811@gmail.com>
2023-01-07 19:03:33 +0200

    random main page offers

--- src/Services/httpService.js
+++ src/Services/httpService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const HTTP = axios.create({
 	baseURL: process.env.REACT_APP_API_BASE_URL,
-	// baseURL: "https://bluepages.sa/api/",
+	// baseURL: "https://bluepages.com.sa/api/",
 	headers: {
 		common: {
 			"Access-Control-Allow-Origin": "*",
